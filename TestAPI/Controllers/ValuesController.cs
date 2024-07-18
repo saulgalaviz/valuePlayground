2a3ed190664565a1ad78cdbc4b809e4532f795ec
saulgalaviz <nostramagear@gmail.com>
2024-07-17 17:39:21 -0700

    Development up until creating Get endpoint

---
+++ TestAPI/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestAPI.Models;
+
+namespace TestAPI.Controllers
+{
+    [Route("api/Values")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Test> GetTests()
+        {
+            return new List<Test>
+            {
+                new Test { Id = 1, Name = "Pool" },
+                new Test { Id = 2, Name = "Jacuzzi" }
+            };
+        }
+    }
+}
